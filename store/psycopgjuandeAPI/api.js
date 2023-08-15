a42b26034c39dcbced26e27792559445587f31fd
Crowdbotics <engineering@crowdbotics.com>
2023-08-15 13:27:37 +0000

    Update store.

--- store/psycopgjuandeAPI/api.js
+++ store/psycopgjuandeAPI/api.js
@@ -1,96 +1,94 @@
-import axios from "axios";
+import axios from "axios"
 const psycopgjuandeAPI = axios.create({
   baseURL: "https://psycopgjuan0814-de-109178.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return psycopgjuandeAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return psycopgjuandeAPI.post(`/api/v1/login/`, payload);
+  return psycopgjuandeAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_newmo_list(payload) {
-  return psycopgjuandeAPI.get(`/api/v1/newmo/`);
+  return psycopgjuandeAPI.get(`/api/v1/newmo/`)
 }
-
 function api_v1_newmo_create(payload) {
-  return psycopgjuandeAPI.post(`/api/v1/newmo/`, payload);
+  return psycopgjuandeAPI.post(`/api/v1/newmo/`, payload)
 }
-
 function api_v1_newmo_retrieve(payload) {
-  return psycopgjuandeAPI.get(`/api/v1/newmo/${payload.id}/`);
+  return psycopgjuandeAPI.get(`/api/v1/newmo/${payload.id}/`)
 }
-
 function api_v1_newmo_update(payload) {
-  return psycopgjuandeAPI.put(`/api/v1/newmo/${payload.id}/`, payload);
+  return psycopgjuandeAPI.put(`/api/v1/newmo/${payload.id}/`, payload)
 }
-
 function api_v1_newmo_partial_update(payload) {
-  return psycopgjuandeAPI.patch(`/api/v1/newmo/${payload.id}/`, payload);
+  return psycopgjuandeAPI.patch(`/api/v1/newmo/${payload.id}/`, payload)
 }
-
 function api_v1_newmo_destroy(payload) {
-  return psycopgjuandeAPI.delete(`/api/v1/newmo/${payload.id}/`);
+  return psycopgjuandeAPI.delete(`/api/v1/newmo/${payload.id}/`)
 }
-
 function api_v1_signup_create(payload) {
-  return psycopgjuandeAPI.post(`/api/v1/signup/`, payload);
+  return psycopgjuandeAPI.post(`/api/v1/signup/`, payload)
 }
-
+function modules_articles_article_list(payload) {
+  return psycopgjuandeAPI.get(`/modules/articles/article/`)
+}
+function modules_articles_article_create(payload) {
+  return psycopgjuandeAPI.post(`/modules/articles/article/`, payload)
+}
+function modules_articles_article_retrieve(payload) {
+  return psycopgjuandeAPI.get(`/modules/articles/article/${payload.id}/`)
+}
+function modules_articles_article_update(payload) {
+  return psycopgjuandeAPI.put(
+    `/modules/articles/article/${payload.id}/`,
+    payload
+  )
+}
+function modules_articles_article_partial_update(payload) {
+  return psycopgjuandeAPI.patch(
+    `/modules/articles/article/${payload.id}/`,
+    payload
+  )
+}
+function modules_articles_article_destroy(payload) {
+  return psycopgjuandeAPI.delete(`/modules/articles/article/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
-  return psycopgjuandeAPI.post(`/rest-auth/login/`, payload);
+  return psycopgjuandeAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return psycopgjuandeAPI.get(`/rest-auth/logout/`);
+  return psycopgjuandeAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return psycopgjuandeAPI.post(`/rest-auth/logout/`);
+  return psycopgjuandeAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return psycopgjuandeAPI.post(`/rest-auth/password/change/`, payload);
+  return psycopgjuandeAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return psycopgjuandeAPI.post(`/rest-auth/password/reset/`, payload);
+  return psycopgjuandeAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return psycopgjuandeAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return psycopgjuandeAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return psycopgjuandeAPI.post(`/rest-auth/registration/`, payload);
+  return psycopgjuandeAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return psycopgjuandeAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return psycopgjuandeAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return psycopgjuandeAPI.get(`/rest-auth/user/`);
+  return psycopgjuandeAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return psycopgjuandeAPI.put(`/rest-auth/user/`, payload);
+  return psycopgjuandeAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return psycopgjuandeAPI.patch(`/rest-auth/user/`, payload);
+  return psycopgjuandeAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -101,6 +99,12 @@ export const apiService = {
   api_v1_newmo_partial_update,
   api_v1_newmo_destroy,
   api_v1_signup_create,
+  modules_articles_article_list,
+  modules_articles_article_create,
+  modules_articles_article_retrieve,
+  modules_articles_article_update,
+  modules_articles_article_partial_update,
+  modules_articles_article_destroy,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
@@ -112,4 +116,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
